fca6910fdf4620003988f810d51bb3c4e39f39c7
ianorego-dev <ian.orego@strathmore.edu>
2026-03-31 13:14:01 +0300

    Add ArrayList, Map and Set collections with file read

--- HospitalStore.java
+++ HospitalStore.java
@@ -14,31 +14,29 @@ public class HospitalStore {
     private Set<String> doctorIds = new HashSet<>();
 
     public void addPatient(Patient p) {
-        String id = p.getPatientID();
-        if (patientIds.contains(id)) return;
+        if (patientIds.contains(p.getPatientID())) return;
         patients.add(p);
-        patientById.put(id, p);
-        patientIds.add(id);
+        patientById.put(p.getPatientID(), p);
+        patientIds.add(p.getPatientID());
     }
 
     public void addDoctor(Doctor d) {
-        String id = d.getStaffID();
-        if (doctorIds.contains(id)) return;
+        if (doctorIds.contains(d.getStaffID())) return;
         doctors.add(d);
-        doctorById.put(id, d);
-        doctorIds.add(id);
+        doctorById.put(d.getStaffID(), d);
+        doctorIds.add(d.getStaffID());
     }
 
     public void addAppointment(Appointment a) {
         appointments.add(a);
     }
 
-    public Patient findPatient(String patientId) {
-        return patientById.get(patientId);
+    public Patient findPatient(String id) {
+        return patientById.get(id);
     }
 
-    public Doctor findDoctor(String staffId) {
-        return doctorById.get(staffId);
+    public Doctor findDoctor(String id) {
+        return doctorById.get(id);
     }
 
     public ArrayList<Patient> getPatients() { return patients; }
@@ -46,100 +44,74 @@ public class HospitalStore {
     public ArrayList<Appointment> getAppointments() { return appointments; }
 
     public void savePatients(String file) throws IOException {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-            for (Patient p : patients) {
-                bw.write(p.getPatientID() + "," + p.getName() + "," + p.getAge() + "," + p.getGender() + "," + p.getIllness());
-                bw.newLine();
-            }
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        for (Patient p : patients) {
+            bw.write(p.getPatientID() + "," + p.getName() + "," + p.getAge() + "," + p.getGender() + "," + p.getIllness());
+            bw.newLine();
         }
+        bw.close();
     }
 
     public void saveDoctors(String file) throws IOException {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-            for (Doctor d : doctors) {
-                bw.write(d.getStaffID() + "," + d.getName() + "," + d.getAge() + "," + d.getGender() + "," + d.getSpecialization());
-                bw.newLine();
-            }
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        for (Doctor d : doctors) {
+            bw.write(d.getStaffID() + "," + d.getName() + "," + d.getAge() + "," + d.getGender() + "," + d.getSpecialization());
+            bw.newLine();
         }
+        bw.close();
     }
 
     public void saveAppointments(String file) throws IOException {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-            for (Appointment a : appointments) {
-                bw.write(a.getAppointmentID() + "," + a.getDate() + "," +
-                        a.getPatient().getPatientID() + "," + a.getDoctor().getStaffID());
-                bw.newLine();
-            }
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        for (Appointment a : appointments) {
+            bw.write(a.getAppointmentID() + "," + a.getDate() + "," + a.getPatient().getPatientID() + "," + a.getDoctor().getStaffID());
+            bw.newLine();
         }
+        bw.close();
     }
 
     public void loadPatients(String file) throws IOException {
         File f = new File(file);
         if (!f.exists()) return;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] x = line.split(",", -1);
-                if (x.length < 5) continue;
-
-                String id = x[0];
-                String name = x[1];
-                int age = Integer.parseInt(x[2]);
-                String gender = x[3];
-                String illness = x[4];
-
-                Patient p = new Patient(name, age, gender, id, illness);
-                addPatient(p);
-            }
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] x = line.split(",");
+            if (x.length < 5) continue;
+            Patient p = new Patient(x[1], Integer.parseInt(x[2]), x[3], x[0], x[4]);
+            addPatient(p);
         }
+        br.close();
     }
 
     public void loadDoctors(String file) throws IOException {
         File f = new File(file);
         if (!f.exists()) return;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] x = line.split(",", -1);
-                if (x.length < 5) continue;
-
-                String staffId = x[0];
-                String name = x[1];
-                int age = Integer.parseInt(x[2]);
-                String gender = x[3];
-                String spec = x[4];
-
-                Doctor d = new Doctor(name, age, gender, staffId, spec);
-                addDoctor(d);
-            }
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] x = line.split(",");
+            if (x.length < 5) continue;
+            Doctor d = new Doctor(x[1], Integer.parseInt(x[2]), x[3], x[0], x[4]);
+            addDoctor(d);
         }
+        br.close();
     }
 
     public void loadAppointments(String file) throws IOException {
         File f = new File(file);
         if (!f.exists()) return;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] x = line.split(",", -1);
-                if (x.length < 4) continue;
-
-                String apptId = x[0];
-                String date = x[1];
-                String patientId = x[2];
-                String doctorId = x[3];
-
-                Patient p = findPatient(patientId);
-                Doctor d = findDoctor(doctorId);
-
-                if (p != null && d != null) {
-                    Appointment a = new Appointment(apptId, date, p, d);
-                    addAppointment(a);
-                }
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] x = line.split(",");
+            if (x.length < 4) continue;
+            Patient p = findPatient(x[2]);
+            Doctor d = findDoctor(x[3]);
+            if (p != null && d != null) {
+                addAppointment(new Appointment(x[0], x[1], p, d));
             }
         }
+        br.close();
     }
 }
